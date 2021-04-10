3f608912e7511e0868fe2b0595a210ba4d4da9d5
diyawaghmare <56413380+diyawaghmare@users.noreply.github.com>
2021-04-10 20:05:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+//Typing animation script
+
+var typed = new Typed(".typing", {
+    strings: ["Cultural Societies of IIIT-A."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
